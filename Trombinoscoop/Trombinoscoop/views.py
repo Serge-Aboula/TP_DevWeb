18bd7e19906ee371dd0b15424af31f5b25b640a9
Serge-Aboula <sergeongolo@gmail.com>
2023-12-01 23:06:30 +0100

    Fichier views.py: redéfinition de la fonction welcome() en déplaçant tout le code HTML dans un template, utilisant la fonction render() et un élément variable HTML

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -1,16 +1,20 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def welcome(request):
-    return HttpResponse('''\
-    <!DOCTYPE html>\
-    <html lang="fr">\
-    <head>\
-        <meta charset="UTF-8">\
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-        <title>Trombinoscoop</title>\
-    </head>\
-    <body>\
-        <p>Bienvenue sur Trombinoscoop !\
-    </body>\
-    </html>                    ''')
+    return render(request, 'my_template.html',
+                  {'logged_user_name': 'Pierre'})
+
+#def welcome(request):
+    #return HttpResponse('''\
+    #<!DOCTYPE html>\
+    #<html lang="fr">\
+    #<head>\
+        #<meta charset="UTF-8">\
+        #<meta name="viewport" content="width=device-width, initial-scale=1.0">\
+        #<title>Trombinoscoop</title>\
+    #</head>\
+    #<body>\
+        #<p>Bienvenue sur Trombinoscoop !\
+    #</body>\
+    #</html>                    ''')
