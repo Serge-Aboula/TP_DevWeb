35d8afdafbee3ed326fa1ea401eda34792218e5f
Serge-Aboula <sergeongolo@gmail.com>
2023-12-02 00:28:59 +0100

    Transmission d'un argument au template welcome.html

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -1,20 +1,6 @@
-from django.http import HttpResponse
+from datetime import datetime
 from django.shortcuts import render
 
 def welcome(request):
-    return render(request, 'my_template.html',
-                  {'logged_user_name': 'Pierre'})
-
-#def welcome(request):
-    #return HttpResponse('''\
-    #<!DOCTYPE html>\
-    #<html lang="fr">\
-    #<head>\
-        #<meta charset="UTF-8">\
-        #<meta name="viewport" content="width=device-width, initial-scale=1.0">\
-        #<title>Trombinoscoop</title>\
-    #</head>\
-    #<body>\
-        #<p>Bienvenue sur Trombinoscoop !\
-    #</body>\
-    #</html>                    ''')
+    return render(request, 'welcome.html',
+                  {'current_date_time': datetime.now()})
