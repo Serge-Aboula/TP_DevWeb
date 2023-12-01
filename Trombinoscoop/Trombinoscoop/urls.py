4c7d3719b97b18a5db7876588c3f8e6c0adc2aa4
Serge-Aboula <sergeongolo@gmail.com>
2023-12-01 21:40:05 +0100

    Fichier urls.py: ajout de path('welcome/', welcome) et importation de la fonction welcome de views.py

--- Trombinoscoop/Trombinoscoop/urls.py
+++ Trombinoscoop/Trombinoscoop/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Trombinoscoop.Trombinoscoop.views import welcome
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/', welcome),    
 ]
