1a4ca6793a0145efda9a7a5cd2dab491a9cbc4c8
Serge-Aboula <sergeongolo@gmail.com>
2023-12-02 10:23:30 +0100

    Correction: datetime.now plutôt comme valeur de la variable de contexte 'current_date_time'

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request, 'welcome.html',
-                  {'current_date_time': datetime.now()})
+                  {'current_date_time': datetime.now})
