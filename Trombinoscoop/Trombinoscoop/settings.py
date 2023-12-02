8f97b8b980ca64334857ae4c2e1315bc9371b846
Serge-Aboula <sergeongolo@gmail.com>
2023-12-02 23:19:53 +0100

    Fichier settings.py: configuration de l'emplacement des fichiers statiques globaux (STATICFILES_DIRS)

--- Trombinoscoop/Trombinoscoop/settings.py
+++ Trombinoscoop/Trombinoscoop/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
