5be17bdf0b543f83b135a61828237407032f60f6
Serge-Aboula <sergeongolo@gmail.com>
2023-12-02 22:51:29 +0100

    Fichier views.py: définition de la fonction login() faisant au template 'login.html'

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def welcome(request):
     return render(request, 'welcome.html',
                   {'current_date_time': datetime.now})
+    
+def login(request):
+    return render(request, 'login.html')
