65ee224c7593a36e18998b2f0e20cece14328b94
Serge-Aboula <sergeongolo@gmail.com>
2023-12-03 17:15:14 +0100

    Ajout de la validation de l'email et du mot de passe dans la classe LoginForm du fichier forms.py

--- Trombinoscoop/Trombinoscoop/forms.py
+++ Trombinoscoop/Trombinoscoop/forms.py
@@ -3,5 +3,16 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.CharField(label='Mot de passe', widget=forms.PasswordInput)
+    
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        email = cleaned_data.get("email")
+        password = cleaned_data.get("password")
+        
+        if email and password:
+            if email != 'a@b.ga' and password != '1':
+                raise forms.ValidationError("Email ou mot de passe invalide.")
+        
+        return cleaned_data
 
 
