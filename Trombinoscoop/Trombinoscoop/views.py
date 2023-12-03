d363e29bc27a3aa8c25d6df68f0b095feb1169e8
Serge-Aboula <sergeongolo@gmail.com>
2023-12-03 15:00:17 +0100

    Utilisation du LoginForm définit dans forms.py, dans la vue: views.py

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -1,28 +1,34 @@
 from datetime import datetime
 from django.shortcuts import redirect, render
 
+from Trombinoscoop.forms import LoginForm
+
 def welcome(request):
     return render(request, 'welcome.html',
                   {'current_date_time': datetime.now})
-    
+
 def login(request):
-    # Teste si le formulaire a été envoyé
-    if not request.POST:
-        # Le formulaire n'a pas été envoyé
-        return render(request, 'login.html')
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
     
-    # Teste si les paramètres attendus ont été transmis
-    if 'email' not in request.POST or 'password' not in request.POST:
-        error = "Veuillez entrer un email et un mot de passe."
-        return render(request, 'login.html', {'error': error})
+# def login(request):
+#     # Teste si le formulaire a été envoyé
+#     if not request.POST:
+#         # Le formulaire n'a pas été envoyé
+#         return render(request, 'login.html')
     
-    email = request.POST['email']
-    password = request.POST['password']
+#     # Teste si les paramètres attendus ont été transmis
+#     if 'email' not in request.POST or 'password' not in request.POST:
+#         error = "Veuillez entrer un email et un mot de passe."
+#         return render(request, 'login.html', {'error': error})
     
-    #Teste si le mot de passe est le bon
-    if password != '1' or email != 'a@b.c':
-       error = "Email ou mot de passe invalide"
-       return render(request, 'login.html', {'error': error}) 
+#     email = request.POST['email']
+#     password = request.POST['password']
     
-    # Tout est bon on va à la page d'accueil
-    return redirect('/welcome')
+#     #Teste si le mot de passe est le bon
+#     if password != '1' or email != 'a@b.c':
+#        error = "Email ou mot de passe invalide"
+#        return render(request, 'login.html', {'error': error}) 
+    
+#     # Tout est bon on va à la page d'accueil
+#     return redirect('/welcome')
