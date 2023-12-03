515105a4c667699f34c9374625f31cf752761ba1
Serge-Aboula <sergeongolo@gmail.com>
2023-12-03 13:52:28 +0100

    Traitement du formulaire du template login.html dans views.py

--- Trombinoscoop/Trombinoscoop/views.py
+++ Trombinoscoop/Trombinoscoop/views.py
@@ -1,9 +1,28 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 def welcome(request):
     return render(request, 'welcome.html',
                   {'current_date_time': datetime.now})
     
 def login(request):
-    return render(request, 'login.html')
+    # Teste si le formulaire a été envoyé
+    if not request.POST:
+        # Le formulaire n'a pas été envoyé
+        return render(request, 'login.html')
+    
+    # Teste si les paramètres attendus ont été transmis
+    if 'email' not in request.POST or 'password' not in request.POST:
+        error = "Veuillez entrer un email et un mot de passe."
+        return render(request, 'login.html', {'error': error})
+    
+    email = request.POST['email']
+    password = request.POST['password']
+    
+    #Teste si le mot de passe est le bon
+    if password != '1' or email != 'a@b.c':
+       error = "Email ou mot de passe invalide"
+       return render(request, 'login.html', {'error': error}) 
+    
+    # Tout est bon on va à la page d'accueil
+    return redirect('/welcome')
