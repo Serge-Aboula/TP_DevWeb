bd24457a349564d2b56113cf773439c696496b6a
Serge-Aboula <sergeongolo@gmail.com>
2023-12-03 23:15:44 +0100

    models.py : Ajout du modèle Message, de la relation friends dans le modèle Person, des modèles Faculty, Campus, Job et Cursus

--- Trombinoscoop/Trombinoscoop/models.py
+++ Trombinoscoop/Trombinoscoop/models.py
@@ -2,7 +2,6 @@
 
 from django.db import models
 
-
 class Person(models.Model):
     registration_number = models.CharField(max_length=10)
     last_name = models.CharField(max_length = 30)
@@ -13,5 +12,25 @@ class Person(models.Model):
     cell_phone_number = models.CharField(max_length = 20)
     # Dans un cas réel, nous ne devrions pas stocker le mot de passe en clair.
     password = models.CharField(max_length=32)
+    friends = models.ManyToManyField('self')
+
+class Message(models.Model):
+    author = models.ForeignKey('Person')
+    content = models.TextField()
+    publication_date = models.DateField()
+
+class Faculty(models.Model):
+    name = models.CharField(max_length = 30)
+    color = models.CharField(max_length = 6)
+    
+class Campus(models.Model):
+    name = models.CharField(max_length = 30)
+    address = models.CharField(max_length = 60)
+    
+class Job(models.Model):
+    title = models.CharField(max_length = 30)
+    
+class Cursus(models.Model):
+    title = models.CharField(max_length = 30)
     
     
