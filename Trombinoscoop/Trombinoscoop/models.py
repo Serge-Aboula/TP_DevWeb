cb2a28605e4a42fb4783bf1b32e7489f249252f8
Serge-Aboula <sergeongolo@gmail.com>
2023-12-05 11:52:24 +0100

    Redéfinition de la méthode __str__ dans les classes modèles définies dans models.py

--- Trombinoscoop/Trombinoscoop/models.py
+++ Trombinoscoop/Trombinoscoop/models.py
@@ -14,25 +14,47 @@ class Person(models.Model):
     password = models.CharField(max_length=32)
     friends = models.ManyToManyField('self')
     faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
 
 class Message(models.Model):
     author = models.ForeignKey('Person', on_delete=models.CASCADE)
     content = models.TextField()
     publication_date = models.DateField()
+    
+    def __str__(self):
+        if len(self.content) > 20:
+            return self.content[:20] + '...'
+        
+        return self.content
+    
 
 class Faculty(models.Model):
     name = models.CharField(max_length = 30)
     color = models.CharField(max_length = 6)
     
+    def __str__(self) -> str:
+        return self.name
+    
 class Campus(models.Model):
     name = models.CharField(max_length = 30)
     address = models.CharField(max_length = 60)
     
+    def __str__(self) -> str:
+        return self.name
+    
 class Job(models.Model):
     title = models.CharField(max_length = 30)
     
+    def __str__(self) -> str:
+        return self.title
+    
 class Cursus(models.Model):
     title = models.CharField(max_length = 30)
+    
+    def __str__(self) -> str:
+        return self.title
     
 class Employee(Person):
     office = models.CharField(max_length = 30)
